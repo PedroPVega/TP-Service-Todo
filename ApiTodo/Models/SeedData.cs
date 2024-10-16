4d0a78b3d42f38546b21e6218b2eb979074c695b
PedroPVega <pvegafernan@ensc.fr>
2024-10-16 15:03:19 +0200

    Etape 2

---
+++ ApiTodo/Models/SeedData.cs
@@ -0,0 +1,22 @@
+// Add todos
+Todo carson = new Todo
+{
+    Id = 1,
+    Task = "Nicolas Corney",
+    Completed = true,
+    Deadline = DateTime.Parse("2016-09-01"),
+};
+Todo carson = new Todo
+{
+    Id = 2,
+    Task = "Anthony Bonar",
+    Completed = true,
+    Deadline = DateTime.Parse("2018-09-09"),
+};
+Todo carson = new Todo
+{
+    Id = 3,
+    Task = "Eudeez",
+    Completed = false,
+    Deadline = DateTime.Parse("2024-16-10"),
+};
